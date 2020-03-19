6f03585c9b00a5132cac4ad2514289d1f633195d
vwpo <vwpo@roborox.org>
2020-03-19 15:36:45 +0300

    feat(*): add new props for infinite list

--- src/infinite-list/index.tsx
+++ src/infinite-list/index.tsx
@@ -1,19 +1,32 @@
 import React, { useEffect, useMemo } from "react"
 import { Atom } from "@grammarly/focal"
 import { createLoadNext, ListPartLoader } from "./create-load-next"
-import { ListLoader } from "./list-loader"
+import { ListLoader, ListLoaderProps } from "./list-loader"
 import { InfiniteListState } from "./domain"
 
-export interface InfiniteListProps<T, C> {
+export interface InfiniteListProps<T, C> extends Omit<ListLoaderProps<T, C>, "error" | "children"> {
 	state: Atom<InfiniteListState<T, C>>
 	loader: ListPartLoader<T, C>
-	loading?: React.ReactNode,
-	error?: (err: any, loadNext: () => void) => React.ReactNode,
-	children?: (loadNext: () => void) => React.ReactNode
+	loadNextRef?: React.MutableRefObject<(() => Promise<void>) | undefined>,
+	children?: (loadNext: () => void) => React.ReactNode,
+	error?: (e: any, reload: () => Promise<void>) => React.ReactNode
 }
 
-export function InfiniteList<T, C>({ state, loader, loading, error, children }: InfiniteListProps<T, C>) {
+export function InfiniteList<T, C>({
+	state,
+	loader,
+	loading,
+	error,
+	loadNextRef,
+	children,
+}: InfiniteListProps<T, C>) {
 	const load = useMemo(() => createLoadNext(loader, state), [state, loader])
+
+	useEffect(() => {
+		if (loadNextRef) {
+			loadNextRef.current = load
+		}
+	}, [load, loadNextRef])
 
 	useEffect(() => {
 		const current = state.get()
--- src/loader/index.tsx
+++ src/loader/index.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react"
+import React, { useMemo, useEffect } from "react"
 import { Observable } from "rxjs"
 import { useRx } from "../use-rx"
 import { LoadingStatus } from "../loading-state"
@@ -9,16 +9,26 @@ export interface LoaderProps {
 	idle?: React.ReactNode
 	loading?: React.ReactNode
 	error?: (error: any) => React.ReactNode
+	onError?: (error: any) => void
 	children: React.ReactNode
 }
 
-export function Loader({ status, children, idle, loading, error }: LoaderProps): React.ReactElement {
+export function Loader({ children, status, onError, ...restProps }: LoaderProps): React.ReactElement {
 	const rx = useMemo(() => caseWhen(status, {
-		loading,
-		idle,
-		error,
 		success: children,
-	}), [status, children, error, idle, loading])
+		...restProps,
+	}), [status, children, Object.values(restProps)])
+
+	useEffect(() => {
+		if (onError) {
+			const s = status.subscribe((next) => {
+				if (next.status === "error") {
+					onError(next.error)
+				}
+			})
+			return () => s.unsubscribe()
+		}
+	}, [onError, status])
 
 	return <>{useRx(rx)}</>
 }
