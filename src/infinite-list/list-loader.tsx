4af198d1940923abae2c96454d692f6235363efb
roborox <dev@roborox.org>
2020-03-27 11:48:50 +0300

    fix(*): useFirstLoadState hook changed to getFirstStatus

--- src/infinite-list/list-loader.tsx
+++ src/infinite-list/list-loader.tsx
@@ -1,16 +1,16 @@
-import React, { useMemo } from "react"
+import React from "react"
 import { Observable } from "rxjs"
 import { map } from "rxjs/operators"
 import { Atom } from "@grammarly/focal"
 import { InfiniteListState } from "./domain"
 import { Cases } from "../case-when"
-import { loadingStatusSuccess, LoadingStatus } from "../loading-state"
+import { LoadingStatus, loadingStatusSuccess } from "../loading-state"
 import { Loader } from "../loader"
 
-export function useFirstLoadState<T, K>(state: Observable<InfiniteListState<T, K>>) {
-	return useMemo(() => state.pipe<LoadingStatus>(map(
+export function getFirstStatus<T, K>(state: Observable<InfiniteListState<T, K>>) {
+	return state.pipe<LoadingStatus>(map(
 		state => state.items.length === 0 ? state.status : loadingStatusSuccess,
-	)), [state])
+	))
 }
 
 export interface ListLoaderProps<D, C> extends Omit<Cases<React.ReactNode>, "success"> {
@@ -19,6 +19,5 @@ export interface ListLoaderProps<D, C> extends Omit<Cases<React.ReactNode>, "suc
 }
 
 export function ListLoader<D, C>({ state, children, ...restProps }: ListLoaderProps<D, C>): React.ReactElement {
-	const firstLoadState = useFirstLoadState(state)
-	return <Loader status={firstLoadState} {...restProps}>{children}</Loader>
+	return <Loader status={getFirstStatus(state)} {...restProps}>{children}</Loader>
 }
