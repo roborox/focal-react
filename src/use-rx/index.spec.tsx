893e1c2c1b686e8ba37840bcf638077afb5c4d8a
Roborox dev <dev@roborox.org>
2020-05-09 14:42:07 +0300

    chore(*): tests

--- src/use-rx/index.spec.tsx
+++ src/use-rx/index.spec.tsx
@@ -1,9 +1,10 @@
-import { Observable, ReplaySubject } from "rxjs"
+import { Observable, ReplaySubject, of } from "rxjs"
 import React from "react"
 import { render } from "@testing-library/react"
 import { Atom } from "@grammarly/focal"
 import { act } from "react-dom/test-utils"
 import { useRx } from "./"
+import { delay, flatMap, map } from "rxjs/operators"
 
 const RxText = ({value, renders}: { value: Observable<string>, renders: Atom<number> }) => {
 	const simple = useRx(value)
@@ -41,16 +42,34 @@ describe("useRx", () => {
 	})
 
 	test("should listen to Atom changes", () => {
+		testSimple(a => a)
+	})
+
+	test("should work with simple map operator", () => {
+		testSimple(a => a.pipe(map(x => x)))
+	})
+
+	test("should work with simple flatMap operator", () => {
+		testSimple(a => a.pipe(flatMap(x => of(x))))
+	})
+
+	test("should not work when there is no immediate value", () => {
+		expect(() => {
+			testSimple(a => a.pipe(delay(100)))
+		}).toThrow()
+	})
+
+	function testSimple(preprocessor: (o: Observable<string>) => Observable<string>) {
 		const renders = Atom.create(0)
 		const text = Math.random().toString()
 		const atom = Atom.create(text)
-		const r = render(<RxText value={atom} renders={renders}/>)
+		const r = render(<RxText value={preprocessor(atom)} renders={renders}/>)
 		expect(r.getByTestId("value")).toHaveTextContent(text)
 		const nextText = Math.random().toString()
 		act(() => atom.set(nextText))
 		expect(r.getByTestId("value")).toHaveTextContent(nextText)
 		expect(renders.get()).toStrictEqual(2)
-	})
+	}
 
 	test("should not trigger rerender if not changed", () => {
 		const subject = new ReplaySubject<number>(1)
