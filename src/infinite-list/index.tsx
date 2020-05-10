f8b1535bdda55223135db1e845f653a2b4f4df4d
Roborox dev <dev@roborox.org>
2020-05-10 14:40:52 +0300

    fix(*): show children in InfiniteList when loading is not defined

--- src/infinite-list/index.tsx
+++ src/infinite-list/index.tsx
@@ -8,16 +8,17 @@ import { useInfiniteList } from "./use-infinite-list"
 export interface InfiniteListProps<T, C> extends Omit<ListLoaderProps<T, C>, "error" | "children"> {
 	state: Atom<InfiniteListState<T, C>>
 	loader: ListPartLoader<T, C>
-	children?: (load: () => Promise<void>) => React.ReactNode,
+	children: (load: () => Promise<void>) => React.ReactNode,
 	error?: (e: any, reload: () => Promise<void>) => React.ReactNode
 }
 
-export function InfiniteList<T, C>({ state, loader, error, children, ...rest }: InfiniteListProps<T, C>) {
+export function InfiniteList<T, C>({ state, loader, error, children, loading, ...rest }: InfiniteListProps<T, C>) {
 	const load = useInfiniteList(state, loader)
+	const realLoading = loading || children(load)
 
 	return (
-		<ListLoader state={state} error={e => error?.(e, load)} {...rest}>
-			{children?.(load)}
+		<ListLoader state={state} error={e => error?.(e, load)} loading={realLoading} {...rest}>
+			{children(load)}
 		</ListLoader>
 	)
 }
